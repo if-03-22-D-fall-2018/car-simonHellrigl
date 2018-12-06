3b6da77d3eefdbed74806434fe45db7067b7782b
simonHellrigl <simsi.hellrigl@gmail.com>
2018-12-06 09:15:30 +0100

    save

---
+++ car.cpp
@@ -0,0 +1,62 @@
+#include "car.h"
+
+struct CarImplementation
+{
+    enum CarType car_type;
+    enum Color color;
+    bool is_available;
+    int fill_level;
+    double acceleration_rate;
+    int speed;
+
+};
+
+
+
+Car get_car(enum CarType)
+{
+
+
+
+
+}
+enum CarType get_type( Car car)
+{
+
+
+}
+
+
+
+enum Color get_color( Car car)
+{
+
+
+}
+int  get_fill_level( Car car)
+{
+
+
+}
+double get_acceleration_rate( Car car)
+{
+
+
+}
+int get_speed(Car car)
+{
+
+
+}
+void set_acceleration_rate(Car car, int rate)
+{
+
+}
+void accelerate(Car car)
+{
+
+}
+void init()
+{
+
+}
